303da53d3cb18dc72cfbab1c014089a98d6062f2
Jon Malmaud <malmaud@gmail.com>
2013-12-08 16:44:30 -0500

    First pass at module dependenciles

--- src/Autoreload.jl
+++ src/Autoreload.jl
@@ -4,12 +4,14 @@ export arequire, areload, amodule, aimport
 
 const files = (String=>Float64)[]
 const module_watch = Symbol[]
+const dependencies = (String=>Vector{String})[]
 verbose_level = :warn
 state = :on
 
 function amodule(module_name)
   push!(module_watch, module_name)
 end
+
 
 function reload_mtime(filename)
   path = Base.find_in_node1_path(filename)
@@ -20,12 +22,18 @@ function reload_mtime(filename)
   m
 end
 
-function arequire(filename="", command= :on)
+function arequire(filename=""; command= :on, depends_on=String[])
   if isempty(filename)
     return collect(keys(files))
   end
   if command == :on
     files[filename] = reload_mtime(filename)
+    dependencies[filename] = depends_on
+    for d in depends_on
+      if !haskey(files, d)
+        arequire(d)
+      end
+    end
     require(filename)
   elseif command == :off
     if haskey(files, filename)
@@ -37,8 +45,8 @@ function arequire(filename="", command= :on)
   return
 end
 
-function aimport(filename)
-  arequire(string(filename))
+function aimport(filename; kwargs...)
+  arequire(string(filename); kwargs...)
   amodule(symbol(filename))
   return
 end
@@ -122,6 +130,53 @@ function switch_mods(vars, var_names, mod1, mod2)
   end
 end
 
+function module_load_order()
+  order = topological_sort(module_watch)
+  return order
+end
+
+function topological_sort{T}(outgoing::Dict{T, Vector{T}})
+  # kahn topological sort
+  # todo: use better data structures
+  order = T[]
+  incoming = (T=>Vector{T})[]
+  for (n, children) in outgoing
+    for child in children
+      if haskey(incoming, child)
+        push!(incoming[child], n)
+      else
+        incoming[child] = [n]
+      end
+    end
+    if !haskey(incoming, n)
+      incoming[n] = T[]
+    end
+  end
+  S = T[]
+  outgoing_counts = (T=>Int)[]
+  for (child, parents) in outgoing
+    if isempty(parents)
+      push!(S, child)
+    end
+    outgoing_counts[child] = length(parents)
+  end
+  while !isempty(S)
+    n = pop!(S)
+    push!(order, n)
+    children = incoming[n]
+    for child in children
+      outgoing_counts[child] -= 1
+      if outgoing_counts[child] == 0
+        push!(S, child)
+      end
+    end
+  end
+  if length(order) != length(outgoing)
+    error("Cyclic dependencies detected")
+  end
+  return order
+end
+
 function deep_reload(file)
   vars = [Main.(_) for _ in names(Main)]
   local mod_olds
@@ -141,6 +196,25 @@ function deep_reload(file)
   end
 end
 
+function try_reload(file)
+  try
+    deep_reload(file)
+  catch err
+    if verbose_level == :warn
+      warn("Could not autoreload $(file):\n$(err.msg)")
+    end
+  end
+end
+
+function is_equal_dict(d1::Dict, d2::Dict)
+  for key in keys(d1)
+    if d1[key]!=d2[key]
+      return false
+    end
+  end
+  return true
+end
+
 function areload(command= :force)
   global state
   if command == :use_state
@@ -153,17 +227,33 @@ function areload(command= :force)
   elseif command == :on
     state = :on
   end
-
-  for (file, file_time) in files
+  file_order = topological_sort(dependencies)
+  should_reload = [filename=>false for filename in file_order]
+  for (i, file) in enumerate(file_order)
+    file_time = files[file]
     if reload_mtime(file) > file_time
-      try
-        deep_reload(file)
-      catch err
-        if verbose_level == :warn
-          warn("Could not autoreload $(file):\n$(err.msg)")
+      should_reload[file] = true
+      files[file] = reload_mtime(file)
+    end
+  end
+  old_reload = copy(should_reload)
+  while true
+    for (child, parents) in dependencies
+      for parent in parents
+        if should_reload[parent]
+          should_reload[child] = true
         end
       end
-      files[file] = reload_mtime(file)
+    end
+    if is_equal_dict(old_reload, should_reload)
+      break
+    end
+    old_reload = copy(should_reload)
+  end
+
+  for file in file_order
+    if should_reload[file]
+      try_reload(file)
     end
   end
   return
