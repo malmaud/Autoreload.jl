2ca262e1e31b32af223f8fcd42affa5c06a89caf
David Anthoff <anthoff@berkeley.edu>
2015-03-16 12:55:00 +0100

    Use compat package for Dict creation

--- src/constants.jl
+++ src/constants.jl
@@ -10,6 +10,6 @@ type Dep
 end
 suppress_warnings = false
 const files = Dict{String,AFile}()
-const options = Dict{Symbol,Any}(:constants=>false, :strip_types=>true, :smart_types=>true, :verbose_level=>:warn, :state=>:on)
+const options = @compat Dict{Symbol,Any}(:constants=>false, :strip_types=>true, :smart_types=>true, :verbose_level=>:warn, :state=>:on)
 # verbose_level = :warn
 # state = :on
