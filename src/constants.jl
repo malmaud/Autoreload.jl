5eab003eeba70e4e69a24b1a500734c1f7682da6
marius <mmillea@ucdavis.edu>
2016-08-29 18:40:09 +0200

    compatible with Julia v0.5

--- src/constants.jl
+++ src/constants.jl
@@ -1,15 +1,15 @@
 type AFile
     should_reload::Bool
     mtime::Float64
-    deps::Vector{UTF8String}
+    deps::Vector{String}
 end
 
 type Dep
     should_reload::Bool
-    name::UTF8String
+    name::String
 end
 suppress_warnings = false
-const files = Dict{UTF8String,AFile}()
+const files = Dict{String,AFile}()
 const options = @compat Dict{Symbol,Any}(:constants=>false, :strip_types=>true, :smart_types=>true, :verbose_level=>:warn, :state=>:on)
 # verbose_level = :warn
 # state = :on
